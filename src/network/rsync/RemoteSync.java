15accc0aa11b79f5295baf42501364b456c21601
zhenya <mogsev@gmail.com>
2015-02-09 16:53:12 +0200

    update Work

--- src/network/rsync/RemoteSync.java
+++ src/network/rsync/RemoteSync.java
@@ -3,18 +3,23 @@ package network.rsync;
 import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
 
 /**
  * Created by zhenya on 06.02.2015.
  */
 public class RemoteSync {
 
-
-
     public RemoteSync() {
         initComponent();
     }
 
+    /**
+     * Initialize components
+     */
     private void initComponent() {
         try {
             //Create input thread
@@ -28,10 +33,42 @@ public class RemoteSync {
         }
     }
 
+    /**
+     * Show list all files on directory
+     * @param dir
+     */
+    private void showDir(String dir) {
+        System.out.println("Show list all files on directory: " + dir);
+        Path path = new File(dir).toPath();
+        try (DirectoryStream<Path> entries = Files.newDirectoryStream(path)) {
+            for (Path entry : entries) {
+                System.out.println(entry.getFileName());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-
-
+        System.out.println("0 - exit;\t1 - show dir;");
         File file = new File(RemoteSyncConfig.dir);
-
+        RemoteSync remoteSync = new RemoteSync();
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            System.out.println("0 - exit;\t1 - show dir;");
+            int intLine = scanner.nextInt();
+            switch (intLine) {
+                case 0:
+                    System.exit(1);
+                    break;
+                case 1:
+                    remoteSync.showDir(RemoteSyncConfig.dir);
+                    break;
+            }
+        }
     }
 }
