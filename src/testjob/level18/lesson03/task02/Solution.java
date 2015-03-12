a2bb6d99dda3f1df339e717d06d9a0e245f84e26
zhenya <mogsev@gmail.com>
2015-03-12 09:14:06 +0200

    at work

---
+++ src/testjob/level18/lesson03/task02/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level18.lesson03.task02;
+
+import java.io.FileInputStream;
+
+/* Минимальный байт
+Ввести с консоли имя файла
+Найти минимальный байт в файле, вывести его на экран.
+Закрыть поток ввода-вывода
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+}
---
+++ src/testjob/level18/lesson03/task04/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.test.level18.lesson03.task04;
+
+import java.io.FileInputStream;
+
+/* Самые редкие байты
+Ввести с консоли имя файла
+Найти байты, которые встречаются в файле меньше всего раз.
+Вывести их на экран через пробел
+Закрыть поток ввода-вывода
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+}
