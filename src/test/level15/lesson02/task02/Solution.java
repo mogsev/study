863dd5cb8ef5b5a4cf86737866451727c1d07776
zhenya <mogsev@gmail.com>
2015-03-17 20:04:54 +0200

    at home

--- src/test/level15/lesson02/task02/Solution.java
+++ src/test/level15/lesson02/task02/Solution.java
@@ -12,6 +12,19 @@ package test.level15.lesson02.task02;
 
 public class Solution {
     //добавьте классы Goose и Dragon тут
+    public static class Goose extends SmallAnimal {
+        @Override
+        public String getSize() {
+            return "Гусь маленький, " + super.getSize();
+        }
+    }
+
+    public static class Dragon extends BigAnimal {
+        @Override
+        public String getSize() {
+            return "Дракон большой, " + super.getSize();
+        }
+    }
 
     public static class BigAnimal {
         protected String getSize() {
