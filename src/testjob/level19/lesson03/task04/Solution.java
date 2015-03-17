04cdcc358b22d14b5f2b0e59d4902ab6dd6dbb9f
zhenya <mogsev@gmail.com>
2015-03-17 15:16:50 +0200

    at work

---
+++ src/testjob/level19/lesson03/task04/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.test.level19.lesson03.task04;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/* И еще один адаптер
+Адаптировать Scanner к PersonScanner.
+Классом-адаптером является PersonScannerAdapter.
+Данные в файле хранятся в следующем виде:
+Иванов Иван Иванович 31 12 1978
+
+Подсказка: воспользуйтесь классом Calendar
+*/
+
+public class Solution {
+    public static class PersonScannerAdapter {
+
+    }
+}
