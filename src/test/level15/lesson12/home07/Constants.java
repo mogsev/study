505e26af135804806a9cb42ef43151e325aeee27
zhenya <mogsev@gmail.com>
2015-03-19 12:01:30 +0200

    at work

--- src/test/level15/lesson12/home07/Constants.java
+++ src/test/level15/lesson12/home07/Constants.java
@@ -1,5 +1,5 @@
 package test.level15.lesson12.home07;
 
 public class Constants {
- //   public static String FILE_NAME = /* add your path to source file here*/;
+    public static String FILE_NAME = "d:/zhenya/test.txt";
 }
--- src/test/level15/lesson12/home07/Solution.java
+++ src/test/level15/lesson12/home07/Solution.java
@@ -1,7 +1,9 @@
 package test.level15.lesson12.home07;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /* Файл в статическом блоке
 1. Инициализируй константу Constants.FILE_NAME полным путем к файлу с данными, который содержит несколько строк.
@@ -11,6 +13,20 @@ import java.util.List;
 
 public class Solution {
     public static List<String> lines = new ArrayList<String>();
+
+    static {
+        try {
+            Scanner scanner = new Scanner(new File(Constants.FILE_NAME));
+            while (scanner.hasNextLine()) {
+                lines.add(scanner.next());
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println(lines);
