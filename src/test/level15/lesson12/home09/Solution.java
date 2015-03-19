ad2e5e4e511e9e2402191c5548820727782ea81e
zhenya <mogsev@gmail.com>
2015-03-19 13:41:32 +0200

    at work

--- src/test/level15/lesson12/home09/Solution.java
+++ src/test/level15/lesson12/home09/Solution.java
@@ -22,9 +22,42 @@ obj name
 double 3.14
 */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Solution {
     public static void main(String[] args) {
         //add your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String str = null;
+        try {
+            str = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String url = str.substring(str.indexOf("?") + 1);
+        String param[] = url.split("&");
+        String obj = null;
+
+        for (String s : param) {
+            try {
+                System.out.print(s.substring(0, s.indexOf("=")) + " ");
+            } catch (Exception ex) {
+                System.out.print(s + " ");
+            }
+            if (s.contains("obj")) {
+                obj = s;
+            }
+        }
+        System.out.println();
+        if (!(obj == null)) {
+            try {
+                alert(Double.parseDouble(obj.substring(obj.indexOf("=") + 1)));
+            } catch (Exception ex) {
+                alert(obj.substring(obj.indexOf("=") + 1));
+            }
+        }
     }
 
     public static void alert(double value) {
